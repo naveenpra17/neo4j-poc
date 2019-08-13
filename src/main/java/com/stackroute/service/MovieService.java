4a5a8afb8d1b0dbf00e101a47567a98303e7172d
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-13 17:50:58 +0530

    Added MovieService and Impl Added MovieServiceInterface Code

--- src/main/java/com/stackroute/service/MovieService.java
+++ src/main/java/com/stackroute/service/MovieService.java
@@ -1,4 +1,14 @@
 package com.stackroute.service;
 
-public class MovieService {
+import com.stackroute.domain.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+
+    Movie getMovieById(int id);
+    Movie saveMovie(Movie movie);
+    List<Movie> getAllMovie();
+    Movie deleteMovieById();
+    Movie updateById();
 }
