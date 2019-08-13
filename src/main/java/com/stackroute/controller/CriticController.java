d5ca35c18a859711275e8c4762d0f0916b032520
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-13 21:34:25 +0530

    Added application.properties

--- src/main/java/com/stackroute/controller/CriticController.java
+++ src/main/java/com/stackroute/controller/CriticController.java
@@ -1,16 +1,24 @@
 package com.stackroute.controller;
 
+import com.stackroute.domain.Critic;
 import com.stackroute.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @RestController
 @RequestMapping(value = "api/v1/")
-public class MovieController {
+public class CriticController {
     private MovieService movieService;
     @Autowired
-    private MovieController(MovieService movieService){
+    private CriticController(MovieService movieService){
         this.movieService=movieService;
     }
+    @GetMapping
+    public Collection<Critic>
+
 }
--- src/main/java/com/stackroute/domain/Critic.java
+++ src/main/java/com/stackroute/domain/Critic.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.neo4j.ogm.annotation.GraphId;
-import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
@@ -23,7 +22,7 @@ public class Critic {
     private String name;
     private int age;
 
-    @Relationship(type= "Rated",direction = Relationship.INCOMING)
+    @Relationship(type = "Rated", direction = Relationship.INCOMING)
     private List<Movie> movies;
 
 }
