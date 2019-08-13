d357a8c4c1e826ed7a462b6eabc83f409274f2fa
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-13 17:43:12 +0530

    added Movies and MovieStars Repository

--- src/main/java/com/stackroute/repository/MovieStarsRepository.java
+++ src/main/java/com/stackroute/repository/MovieStarsRepository.java
@@ -1,4 +1,8 @@
 package com.stackroute.repository;
 
-public interface MovieStarsRepository {
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieStarsRepository extends Neo4jRepository<MovieStarsRepository,Integer> {
 }
--- src/main/java/com/stackroute/repository/MoviesRepository.java
+++ src/main/java/com/stackroute/repository/MoviesRepository.java
@@ -1,4 +1,9 @@
 package com.stackroute.repository;
 
-public interface MoviesRepository {
+import com.stackroute.domain.Movie;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MoviesRepository extends Neo4jRepository<Movie, Integer> {
 }
