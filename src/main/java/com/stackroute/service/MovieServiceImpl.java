d7d827f07e6e27847405dc2417198b965da7a8a2
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-13 17:56:57 +0530

    In MovieService added code for getMovieById

--- src/main/java/com/stackroute/service/MovieServiceImpl.java
+++ src/main/java/com/stackroute/service/MovieServiceImpl.java
@@ -1,4 +1,41 @@
 package com.stackroute.service;
 
+import com.stackroute.domain.Movie;
+import com.stackroute.repository.MoviesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class MovieServiceImpl implements MovieService {
+    private MoviesRepository moviesRepository;
+
+    @Autowired
+    public MovieServiceImpl(MoviesRepository moviesRepository) {
+        this.moviesRepository = moviesRepository;
+    }
+
+    @Override
+    public Movie getMovieById(int id) {
+        return moviesRepository.findById(id).get();
+    }
+
+    @Override
+    public Movie saveMovie(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getAllMovie() {
+        return null;
+    }
+
+    @Override
+    public Movie deleteMovieById() {
+        return null;
+    }
+
+    @Override
+    public Movie updateById() {
+        return null;
+    }
 }
